5276b03c81a2889542cb9adc5a7ad01e1e47845e
royvargas <r.vargas.carrasco@isur.edu.pe>
2025-12-15 17:10:13 -0500

    Cambios en oficina

--- Sigre/Sigre.Movil/config.js
+++ Sigre/Sigre.Movil/config.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 //const baseURL = 'https://localhost:7280/api/'; //
 //const baseURL = 'http://192.168.0.208/SigreHost/api/'; // local Luis
 //const baseURL = 'http://192.168.1.32/SigreHost/api/'; // local Roy
-//const baseURL = 'http://192.168.1.41/SigreHost/api/'; // OFICINA SIGRE
-const baseURL = 'https://balladlike-priscilla-uncaringly.ngrok-free.dev/SigreHost/api/'; // ngrok acceso
+const baseURL = 'http://192.168.1.48/SigreHost/api/'; // OFICINA SIGRE
+//const baseURL = 'https://balladlike-priscilla-uncaringly.ngrok-free.dev/SigreHost/api/'; // ngrok acceso
 export const API_URL = baseURL; 
 
 export const api = () => {
