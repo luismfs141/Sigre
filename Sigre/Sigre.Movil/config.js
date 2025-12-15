7e7ea42ae91499c610b6ba5ea5a40b60953f851a
luismfs141 <luismfs141@gmail.com>
2025-12-14 20:30:02 -0500

    config ngrok

--- Sigre/Sigre.Movil/config.js
+++ Sigre/Sigre.Movil/config.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 //const baseURL = 'https://localhost:44325/api/'; // desar con SSL
 //const baseURL = 'http://localhost:56870/api/'; // desa sin SSL
 //const baseURL = 'https://localhost:7280/api/'; //
-const baseURL = 'http://192.168.0.208/SigreHost/api/'; // local Luis
+//const baseURL = 'http://192.168.0.208/SigreHost/api/'; // local Luis
 //const baseURL = 'http://192.168.1.32/SigreHost/api/'; // local Roy
 //const baseURL = 'http://192.168.1.41/SigreHost/api/'; // OFICINA SIGRE
-
+const baseURL = 'https://balladlike-priscilla-uncaringly.ngrok-free.dev/SigreHost/api/'; // ngrok acceso
 export const API_URL = baseURL; 
 
 export const api = () => {
@@ -16,10 +16,3 @@ export const api = () => {
     headers: { 'Content-Type': 'application/json' },
   });
 }; 
-
-// export const api = axios.create({
-//   baseURL,
-//   headers: { 'Content-Type': 'application/json' },
-// });
-
-
