3616afd12f3cf0136548544e678aecca6b767b01
fmamanifdeveloper <fmamanifdeveloper@gmail.com>
2026-01-07 12:00:11 -0500

    .

--- Sigre/Sigre.Movil/config.js
+++ Sigre/Sigre.Movil/config.js
@@ -4,8 +4,9 @@ import axios from 'axios';
 //const baseURL = 'https://localhost:44325/api/'; // desar con SSL
 //const baseURL = 'http://localhost:56870/api/'; // desa sin SSL
 //const baseURL = 'https://localhost:7280/api/'; //
-const baseURL = 'http://192.168.0.208/SigreHost/api/'; // local Luis
+//const baseURL = 'http://192.168.0.208/SigreHost/api/'; // local Luis
 //const baseURL = 'http://192.168.1.59/SigreHost/api/'; // local Roy
+const baseURL = 'http://192.168.1.41/SigreHost/api/'; //local Cami 
 //const baseURL = 'http://192.168.1.41/SigreHost/api/'; // OFICINA SIGRE
 //const baseURL = 'https://balladlike-priscilla-uncaringly.ngrok-free.dev/SigreHost/api/'; // ngrok acceso
 export const API_URL = baseURL; 
