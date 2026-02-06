402a9b9a5506959cde5115b365dafa315f698951
Manuel Foco <manuel.foco@externo.auna.org>
2026-02-06 07:28:13 -0500

    Ruta injectada Sigre Web

--- Sigre/Sigre.Web/src/api/apiConfig.js
+++ Sigre/Sigre.Web/src/api/apiConfig.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
-// 👉 URL base del backend
-const baseURL = 'http://localhost/SigreHost/api/';
-// Producción
-//const baseURL = 'https://sigre-api-a6bbh4drgpgjeshw.canadacentral-01.azurewebsites.net/api/';
+export const API_URL = process.env.REACT_APP_API_URL;
 
-export const API_URL = baseURL; 
 const api = axios.create({
-  baseURL: baseURL,
-  //timeout: 10000, // un poco más alto por seguridad
+  baseURL: API_URL,
 });
 
 // ✅ Interceptor de solicitud: agrega el token JWT automáticamente
