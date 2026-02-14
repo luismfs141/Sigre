52f7a36071b2b1b118462311f90f46c4ef70e9e7
Manuel Foco <manuel.foco@externo.auna.org>
2026-02-14 05:46:57 -0500

    .

--- Sigre/Sigre.Web/src/api/apiConfig.js
+++ Sigre/Sigre.Web/src/api/apiConfig.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // 👉 URL base del backend
-const baseURL = 'http://localhost/SigreHost/api/';
+//const baseURL = 'http://localhost/SigreHost/api/';
 // Producción
-//const baseURL = 'https://sigre-api-a6bbh4drgpgjeshw.canadacentral-01.azurewebsites.net/api/';
+const baseURL = 'https://sigre-api-a6bbh4drgpgjeshw.canadacentral-01.azurewebsites.net/api/';
 
 export const API_URL = baseURL; 
 const api = axios.create({
