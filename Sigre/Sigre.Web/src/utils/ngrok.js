3c7f8b22664747951cb4a96404dd081c52481109
CAMIANAIS <cmamanicr@unsa.edu.pe>
2026-03-24 21:51:22 -0500

    ngrok y prod cambio envs

--- Sigre/Sigre.Web/src/utils/ngrok.js
+++ Sigre/Sigre.Web/src/utils/ngrok.js
@@ -1,7 +1,7 @@
 // Descomenta la URL que necesites usar
 
 // Servidor estático en local
-export const API_BASE_URL = "http://localhost:8080";
+//export const API_BASE_URL = "http://localhost:8080";
 
 // Servidor Ngrok
-//export const API_BASE_URL = "https://subobscure-hilda-audacious.ngrok-free.dev";
+export const API_BASE_URL = "https://subobscure-hilda-audacious.ngrok-free.dev";
