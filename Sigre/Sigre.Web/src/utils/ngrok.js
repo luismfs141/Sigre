98e287ca50668c2aaf3eec60f98ab216f4859d00
Manuel Foco <manuel.foco@externo.auna.org>
2026-06-05 14:25:38 -0500

    .

--- Sigre/Sigre.Web/src/utils/ngrok.js
+++ Sigre/Sigre.Web/src/utils/ngrok.js
@@ -4,4 +4,4 @@
 //export const API_BASE_URL = "http://localhost:8080";
 
 // Servidor Ngrok
-export const API_BASE_URL = "https://karen-victor-romance-phil.trycloudflare.com/";
+export const API_BASE_URL = "https://ammonium-defame-nicotine.ngrok-free.dev/";
