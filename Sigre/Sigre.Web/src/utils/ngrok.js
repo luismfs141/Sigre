6ccabbdd07b71f882167a260bb0b915bde4d2ba8
Manuel Foco <manuel.foco@externo.auna.org>
2026-06-22 21:21:35 -0500

    .

--- Sigre/Sigre.Web/src/utils/ngrok.js
+++ Sigre/Sigre.Web/src/utils/ngrok.js
@@ -4,4 +4,4 @@
 //export const API_BASE_URL = "http://localhost:8080";
 
 // Servidor Ngrok
-export const API_BASE_URL = " https://suggested-helping-gives-ciao.trycloudflare.com/";
+export const API_BASE_URL = " https://clark-thin-inches-park.trycloudflare.com/";
